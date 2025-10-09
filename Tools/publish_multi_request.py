2a4775b1cecd5c7efd4c0526ed5a27a87fe0db24
nikitosych <174215049+nikitosych@users.noreply.github.com>
2025-10-09 19:38:37 +0200

    tag print

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -77,6 +77,7 @@ def get_files_to_publish() -> Iterable[str]:
 def get_engine_version() -> str:
     proc = subprocess.run(["git", "describe","--tags", "--abbrev=0"], stdout=subprocess.PIPE, cwd="RobustToolbox", check=True, encoding="UTF-8")
     tag = proc.stdout.strip()
+    print(f"Found engine version tag: {tag}")
     assert tag.startswith("v")
     return tag[1:] # Cut off v prefix.
 
