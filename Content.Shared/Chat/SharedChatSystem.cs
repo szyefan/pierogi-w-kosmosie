63fb4410f4faa5dfc5a3606a17d70ccec727ff78
nikitosych <174215049+nikitosych@users.noreply.github.com>
2025-10-09 23:04:42 +0200

    ChatSystem - rozw. konfliktu

--- Content.Shared/Chat/SharedChatSystem.cs
+++ Content.Shared/Chat/SharedChatSystem.cs
@@ -29,7 +29,7 @@ public abstract class SharedChatSystem : EntitySystem
     public const int WhisperClearRange = 2; // how far whisper goes while still being understandable, in world units
     public const int WhisperMuffledRange = 5; // how far whisper goes at all, in world units
     public static readonly SoundSpecifier DefaultAnnouncementSound
-        = new SoundPathSpecifier("/Audio/Announcements/announce.ogg");
+        = new SoundPathSpecifier("/Audio/_Polonium/Announcements/announce.ogg");
 
     public static readonly ProtoId<RadioChannelPrototype> CommonChannel = "Common";
 
